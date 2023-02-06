804615f60e1a61cc9e9405e9823faceba7d820c9
veiga <71990656+gustavovianaveiga@users.noreply.github.com>
2023-02-06 17:18:28 -0300

    lua

--- src/pages/Mirror.jsx
+++ src/pages/Mirror.jsx
@@ -23,23 +23,40 @@ function Mirror() {
     height:80px;
     background:#3838f3;
     border-radius: 100%;
+    z-index:3;
     transition: all 0.2s;
     &:focus-visible { outline: none; }
     &::before { 
                     content:''; 
                     width:150px;
                     height:6px; 
+                    align-self: center;
                     transform:rotate(-20deg);
                     background:linear-gradient(90deg,
                     rgba(255,255,255,1) 0%, rgba(198,198,198,1) 52%,
                     rgba(186,186,186,1) 100%); 
                     position: absolute;
+                    z-index:4;
                     border-radius:50%;
                     box-shadow: 0px 4px 6px 0px rgba(0,0,0,0.75);
                     transition: all 0.3s;}
- &:hover::before { 
+    &:hover::before { 
                     transform:rotate(30deg);
                      }
+    &:after { 
+                    content:''; 
+                    width:5px;
+                    height:5px; 
+                    position: relative;
+                    left:40px;
+                    top:0px;
+                    z-index:2;
+                    border-radius:100%;
+                    background:#b2b2b2;
+                    box-shadow: 0px 4px 6px 0px rgba(0,0,0,0.75);
+                    transition: all 0.3s;}  
+    &:hover::after {left:-60px;
+    top:30px;}                            
     `)
     const [value, setvalue] = useState()
 
@@ -51,7 +68,7 @@ function Mirror() {
     return (
         <Agrupamento className='flex justify-between items-center' >
             <section className='flex flex-col items-center justify-center '>
-                <Square className='flex items-center justify-center'></Square>
+                <Square className='flex justify-center'></Square>
             </section>
             <section id='secoundsection'>
                 <Textareastyled autocorrect="off" autocomplete="off" autocapitalize="off" spellcheck="false" onChange={mirrorcode}>{code}</Textareastyled></section>
